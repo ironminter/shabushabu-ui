a68dd840f8bb6fa89db7148dbd4721e5b83a0fe9
Iwan <langhua292@163.com>
2020-10-17 20:54:45 +0800

    Change the url of twitter.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
         <div className="footer">
             <div className="footer-box">
                 <a href="https://t.me/ShabuShabuFinanceAnnouncement" target="new_tab">Telegram</a>
-                <a href="https://twitter.com/FinanceShabu" target="new_tab">Twitter</a>
+                <a href="https://mobile.twitter.com/shabufinance" target="new_tab">Twitter</a>
                 <a href="https://medium.com/@shabushabu" target="new_tab">Medium</a>
                 <a href="https://github.com/hvo408/shabushabu-ui" target="new_tab">Github</a>
             </div>
