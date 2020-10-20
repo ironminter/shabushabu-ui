72cc19ea3f90c7b67e597e4e85540735b0579431
Huey Vo <hvo408@yahoo.com>
2020-10-19 18:14:22 -0700

    master updated telegram link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <div className="footer">
             <div className="footer-box">
-                <a href="https://t.me/ShabuShabuFinanceAnnouncement" target="new_tab">Telegram</a>
+                <a href="https://t.me/shabushabufinance" target="new_tab">Telegram</a>
                 <a href="https://mobile.twitter.com/shabufinance" target="new_tab">Twitter</a>
                 <a href="https://medium.com/@shabushabu" target="new_tab">Medium</a>
             </div>
