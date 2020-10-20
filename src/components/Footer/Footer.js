a7f248d22c3b539b1433aa9d6ea839e8d4c642b3
josh <j0shua.chen@yahoo.com>
2020-10-19 18:53:35 -0700

    replace text with logos

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Footer.css'
-
 function Footer() {
     return (
-        <div className="footer">
-            <div className="footer-box">
-                <a href="https://t.me/shabushabufinance" target="new_tab">Telegram</a>
-                <a href="https://mobile.twitter.com/shabufinance" target="new_tab">Twitter</a>
-                <a href="https://medium.com/@shabushabu" target="new_tab">Medium</a>
+        <div className='footer'>
+            <div className='footer-box'>
+                <a href='https://t.me/shabushabufinance' target='new_tab'>
+                  <img className='external-logo' src={require('../../assets/images/telegram.svg')} />
+                </a>
+                <a href='https://mobile.twitter.com/shabufinance' target='new_tab'>
+                  <img className='external-logo' src={require('../../assets/images/twitter.svg')} />
+                </a>
+                <a href='https://medium.com/@shabushabu' target='new_tab'>
+                  <img className='external-logo' src={require('../../assets/images/medium.svg')} />
+                </a>
             </div>
         </div>
     );
